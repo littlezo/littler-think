f6444d462aad73870696a5948e9d585c89a22447
@小小只^v^ <littlezov@qq.com>
2021-08-22 16:33:47 +0800

    update: config

--- config/database.php
+++ config/database.php
@@ -74,5 +74,45 @@ return [
 		],
 
 		// 更多的数据库配置信息
+        "source" => [
+			// 数据库类型
+			'type' => Env::get('database.type', 'mysql'),
+			// 服务器地址
+			'hostname' => '8.134.119.14',
+			// 数据库名
+			'database' => 'app_hphk1688_com',
+			// 用户名
+			'username' => 'root',
+			// 密码
+			'password' => 'c705a85d9f445951',
+			// 端口
+			'hostport' => Env::get('database.hostport', '3306'),
+			// 数据库连接参数
+			'params' => [],
+			// 数据库编码默认采用utf8
+			'charset' => Env::get('database.charset', 'utf8mb4'),
+			// 数据库表前缀
+			'prefix' => Env::get('database.prefix', 'lz_'),
+
+			// 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+			'deploy' => 0,
+			// 数据库读写是否分离 主从式有效
+			'rw_separate' => false,
+			// 读写分离后 主服务器数量
+			'master_num' => 1,
+			// 指定从服务器序号
+			'slave_no' => '',
+			// 是否严格检查字段是否存在
+			'fields_strict' => true,
+			// 是否需要断线重连
+			'break_reconnect' => false,
+			// 监听SQL
+			'trigger_sql' => true,
+			// 开启字段缓存
+			'fields_cache' => false,
+			// 字段缓存路径
+			'schema_cache_path' => app()->getRuntimePath() . 'schema' . DIRECTORY_SEPARATOR,
+		],
+
 	],
 ];
